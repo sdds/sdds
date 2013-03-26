d2ad54bb24b3637b370a34b0e23bec452ec8a43c
Kai Beckmann <kai.beckmann@hs-rm.de>
2013-03-26 13:18:44 +0000

    all relevant driver/dds topics included
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@297 08239170-4572-482a-b791-2244fd6e1e4b

--- example/contiki_wallnode/Dependency.mk
+++ example/contiki_wallnode/Dependency.mk
@@ -2,13 +2,22 @@ DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/DoorSensor-ds.o
 DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/LightSensor-ds.o
 DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/MovementSensor-ds.o
 DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/OnOffSwitch-ds.o
-DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/TemperatureAndHumiditySensor-ds.o
+DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/HumiditySensor-ds.o
 DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/TemperatureSensor-ds.o
+DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/DimmableLight-ds.o
+DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/LightRegulationFunctionality-ds.o
+DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/OnOffFunctionality-ds.o
+DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/SimpleLamp-ds.o
+DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/ToggleFunctionality-ds.o
+DATA_DEPEND_OBJS += $(SDDS_OBJDIR)/WindowSensor-ds.o
+
 
 APP_DEPEND_OBJS += $(SDDS_OBJDIR)/Wiedas_WallnodeApp.o
 APP_DEPEND_OBJS += $(SDDS_OBJDIR)/Wiedas_SensorApp_Light.o
 APP_DEPEND_OBJS += $(SDDS_OBJDIR)/Wiedas_SensorApp_Temperature.o
 APP_DEPEND_OBJS += $(SDDS_OBJDIR)/Wiedas_SensorApp_Movement.o
+APP_DEPEND_OBJS += $(SDDS_OBJDIR)/Wiedas_SensorApp_Humidity.o
+
 
 DRIVER_DEPEND_OBJS += $(SDDS_OBJDIR)/sdds-driver-$(SDDS_ARCH)-AMN31112.o
 DRIVER_DEPEND_OBJS += $(SDDS_OBJDIR)/sdds-driver-$(SDDS_ARCH)-TSL2561.o
