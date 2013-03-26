52fecd3d6a81bb317baaf4f02453c1a5d5292848
Kai Beckmann <kai.beckmann@hs-rm.de>
2013-03-26 12:57:09 +0000

    weitere interface methoden für dowork
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@295 08239170-4572-482a-b791-2244fd6e1e4b

--- example/contiki_wallnode/Wiedas_sensor_apps.h
+++ example/contiki_wallnode/Wiedas_sensor_apps.h
@@ -28,32 +28,39 @@ rc_t Wiedas_SensorApp_Movement_process();
 
 rc_t Wiedas_SensorApp_Humidity_init();
 rc_t Wiedas_SensorApp_Humidity_start();
-
-rc_t Wiedas_SensorApp_Humidity_init();
-rc_t Wiedas_SensorApp_Humidity_start();
+rc_t Wiedas_SensorApp_Humidity_dowork();
+rc_t Wiedas_SensorApp_Humidity_process();
 
 rc_t Wiedas_SensorApp_Door_init(uint8_t number);
 rc_t Wiedas_SensorApp_Door_start(uint8_t number);
+rc_t Wiedas_SensorApp_Door_dowork(uint8_t number);
 
 rc_t Wiedas_SensorApp_Window_init(uint8_t number);
 rc_t Wiedas_SensorApp_Window_start(uint8_t number);
+rc_t Wiedas_SensorApp_Window_dowork(uint8_t number);
 
 rc_t Wiedas_SensorApp_Switch_init(uint8_t number);
 rc_t Wiedas_SensorApp_Switch_start(uint8_t number);
+rc_t Wiedas_SensorApp_Switch_dowork(uint8_t number);
 
 rc_t Wiedas_SensorApp_Battery_init();
 rc_t Wiedas_SensorApp_Battery_start();
+rc_t Wiedas_SensorApp_Battery_dowork();
 
 rc_t Wiedas_SensorApp_SimpleLight_init();
 rc_t Wiedas_SensorApp_SimplteLight_start();
+rc_t Wiedas_SensorApp_SimplteLight_dowork();
 
 rc_t Wiedas_SensorApp_OnOffOutput_init();
 rc_t Wiedas_SensorApp_OnOffOutput_start();
+rc_t Wiedas_SensorApp_OnOffOutput_dowork();
 
 rc_t Wiedas_SensorApp_PowerMeter_init();
 rc_t Wiedas_SensorApp_PowerMeter_start();
+rc_t Wiedas_SensorApp_PowerMeter_dowork();
 
 rc_t Wiedas_SensorApp_DimmerLamp_init();
 rc_t Wiedas_SensorApp_DimmerLamp_start();
+rc_t Wiedas_SensorApp_DimmerLamp_dowork();
 
 #endif /* WIEDAS_SENSOR_APPS_H_ */
