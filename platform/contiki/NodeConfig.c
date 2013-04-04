578d8efd156a6eaa8b69a9bdf812cad76422472a
Kai Beckmann <kai.beckmann@hs-rm.de>
2013-04-04 17:19:59 +0000

    prefix für platformtypen für wiedas eingefügt. prefix 1 für dopsy hw prefix 2 für hexabus hw
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@313 08239170-4572-482a-b791-2244fd6e1e4b

--- platform/contiki/NodeConfig.c
+++ platform/contiki/NodeConfig.c
@@ -19,9 +19,19 @@ SSW_NodeID_t NodeConfig_getNodeID(void) {
     for (uint8_t i=0; i < UIP_DS6_ADDR_NB; i++) {
 	if (uip_ds6_if.addr_list[i].isused) {
 	    id = uip_ntohs(uip_ds6_if.addr_list[i].ipaddr.u16[7]);
- 	    Log_debug("node ID: 0x%4x \n ", id);
+ 	    Log_debug("node native ID: 0x%4x \n ", id);
 	}
     }
+#ifdef CONTIKI_TARGET_AVR_ATMEGA128RFA1
+
+    id = (id & 0x0fff) | 0x1000;
+    Log_debug("WieDAS node ID: 0x%4x \n ", id);
+#endif
+
+#ifdef CONTIKI_TARGET_HEXABUS_SOCKET
+    id = (id & 0x0fff) | 0x2000;
+      Log_debug("WieDAS node ID: 0x%4x \n ", id);
+#endif
 
     return id;
 
