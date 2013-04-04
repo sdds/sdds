0c84e68e854ba368096e0028ee9b099a0f459d5c
Kai Beckmann <kai.beckmann@hs-rm.de>
2013-04-04 17:21:30 +0000

    minimales testprogramm für DimmerLampen
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@314 08239170-4572-482a-b791-2244fd6e1e4b

--- example/linux_basic/mywrite.c
+++ example/linux_basic/mywrite.c
@@ -30,16 +30,84 @@ int main(void)
 			return -1;
 		}
 
-		if (strcmp(s, "read_test\n") == 0)
+		if (strcmp(s, "switch_off\n") == 0)
 		{
-			Test t;
-			Test* t_ptr = &t;
+			OnOffFunctionality t;
+			OnOffFunctionality* t_ptr = &t;
 
-			if (DDS_TestDataReader_take_next_sample(g_Test_reader, &t_ptr, NULL) != DDS_RETCODE_NO_DATA)
+			t.command = 0;
+			t.id = 0x18aa;
+
+			if (DDS_OnOffFunctionalityDataWriter_write(g_OnOffFunctionality_writer, t_ptr, NULL) != DDS_RETCODE_NO_DATA)
 			{
-				printf("test value: %d \n", (t.testvalue) );
+				printf("test value: %d device 0x%x \n", (t.command), t.id );
+			}
+		} else if (strcmp(s, "switch_on\n") == 0)
+		{
+			OnOffFunctionality t;
+			OnOffFunctionality* t_ptr = &t;
+
+			t.command = 1;
+			t.id = 0x18aa;
+
+			if (DDS_OnOffFunctionalityDataWriter_write(g_OnOffFunctionality_writer, t_ptr, NULL) != DDS_RETCODE_NO_DATA)
+			{
+				printf("test value: %d device 0x%x \n", (t.command), t.id );
 			}
 		}
+		else if (strcmp(s, "dim100\n") == 0)
+				{
+					LightRegulationFunctionality t;
+					LightRegulationFunctionality* t_ptr = &t;
+
+					t.setValue = 100;
+					t.id = 0x18aa;
+
+					if (DDS_LightRegulationFunctionalityDataWriter_write(g_LightRegulationFunctionality_writer, t_ptr, NULL) != DDS_RETCODE_NO_DATA)
+					{
+						printf("test value: %d device 0x%x \n", (t.setValue), t.id );
+					}
+				}
+		else if (strcmp(s, "dim50\n") == 0)
+						{
+							LightRegulationFunctionality t;
+							LightRegulationFunctionality* t_ptr = &t;
+
+							t.setValue = 50;
+							t.id = 0x18aa;
+
+							if (DDS_LightRegulationFunctionalityDataWriter_write(g_LightRegulationFunctionality_writer, t_ptr, NULL) != DDS_RETCODE_NO_DATA)
+							{
+								printf("test value: %d device 0x%x \n", (t.setValue), t.id );
+							}
+						}
+		else if (strcmp(s, "dim0\n") == 0)
+								{
+									LightRegulationFunctionality t;
+									LightRegulationFunctionality* t_ptr = &t;
+
+									t.setValue = 0;
+									t.id = 0x18aa;
+
+									if (DDS_LightRegulationFunctionalityDataWriter_write(g_LightRegulationFunctionality_writer, t_ptr, NULL) != DDS_RETCODE_NO_DATA)
+									{
+										printf("test value: %d device 0x%x \n", (t.setValue), t.id );
+									}
+								}
+		else if (strcmp(s, "dim\n") == 0)
+										{
+											LightRegulationFunctionality t;
+											LightRegulationFunctionality* t_ptr = &t;
+
+											printf("Value: ");
+											scanf("%u", &(t.setValue));
+											t.id = 0x18aa;
+
+											if (DDS_LightRegulationFunctionalityDataWriter_write(g_LightRegulationFunctionality_writer, t_ptr, NULL) != DDS_RETCODE_NO_DATA)
+											{
+												printf("test value: %d device 0x%x \n", (t.setValue), t.id );
+											}
+										}
 		else if (strcmp(s, "quit\n") == 0)
 		{
 			break;
