6ab56587d8f26026fe1acdd1a5962718068cb9d9
Kai Beckmann <kai.beckmann@hs-rm.de>
2013-04-10 17:22:48 +0000

    added (and commented out) the wiedas application calls
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@329 08239170-4572-482a-b791-2244fd6e1e4b

--- example/contiki_wallnode/Wiedas_WallnodeApp.c
+++ example/contiki_wallnode/Wiedas_WallnodeApp.c
@@ -15,7 +15,7 @@ rc_t SDDS_Application_init() {
 
 	// init light sensor
 
-	//ret = Wiedas_SensorApp_Light_init();
+	ret = Wiedas_SensorApp_Light_init();
 
 	// init temperature sensor
 
@@ -30,7 +30,35 @@ rc_t SDDS_Application_init() {
 	//ret = Wiedas_SensorApp_Humidity_init();
 
 	// init dimmer lamp
-	ret = Wiedas_SensorApp_DimmerLamp_init();
+	//ret = Wiedas_SensorApp_DimmerLamp_init();
+
+	// init door sensors
+	// door on first port
+	//ret = Wiedas_SensorApp_Door_init(0);
+
+	// door on second port
+	//ret = Wiedas_SensorApp_Door_init(1);
+
+	// door on third port
+	//ret = Wiedas_SensorApp_Door_init(2);
+
+	// switch on first port
+	//ret = Wiedas_SensorApp_Switch_init(0);
+
+	// switch on second port
+	//ret = Wiedas_SensorApp_Switch_init(1);
+
+	// switch on third port
+	//ret = Wiedas_SensorApp_Switch_init(2);
+
+	// window on first port
+	//ret = Wiedas_SensorApp_Window_init(0);
+
+	// window on second port
+	//ret = Wiedas_SensorApp_Window_init(1);
+
+	// window on third port
+	//ret = Wiedas_SensorApp_Window_init(2);
 
 	return SDDS_RT_OK;
 }
@@ -40,21 +68,38 @@ rc_t SDDS_Application_doWork(){
 	rc_t ret;
 	//durchreichen der process loop
 
-	/*
+
 	ret = Wiedas_SensorApp_Light_dowork();
 
-	ret = Wiedas_SensorApp_Temperature_dowork();
+	//ret = Wiedas_SensorApp_Temperature_dowork();
 
-	ret = Wiedas_SensorApp_Movement_dowork();
+	//ret = Wiedas_SensorApp_Movement_dowork();
 
-	ret = Wiedas_SensorApp_Humidity_dowork();
-*/
-	ret = Wiedas_SensorApp_DimmerLamp_dowork();
+	//ret = Wiedas_SensorApp_Humidity_dowork();
+
+	//ret = Wiedas_SensorApp_DimmerLamp_dowork();
+
+	//ret = Wiedas_SensorApp_Door_dowork(0);
+	//ret = Wiedas_SensorApp_Door_dowork(1);
+	//ret = Wiedas_SensorApp_Door_dowork(2);
+
+	//ret = Wiedas_SensorApp_Switch_dowork(0);
+	//ret = Wiedas_SensorApp_Switch_dowork(1);
+	//ret = Wiedas_SensorApp_Switch_dowork(2);
+
+	//ret = Wiedas_SensorApp_Window_dowork(0);
+	//ret = Wiedas_SensorApp_Window_dowork(1);
+	//ret = Wiedas_SensorApp_Window_dowork(2);
 
 	return SDDS_RT_OK;
 }
 
 rc_t SDDS_Application_start(){
+
+	rc_t ret;
+
+	//ret = Wiedas_SensorApp_Movement_start();
+
 	return SDDS_RT_OK;
 }
 
