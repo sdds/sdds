61fcb8c0612fab8182ffb16e5e6b1ed5cdc2a8e2
Kai Beckmann <kai.beckmann@hs-rm.de>
2013-04-10 17:19:05 +0000

    wiedas application to handle the movement sensor
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@324 08239170-4572-482a-b791-2244fd6e1e4b

--- example/contiki_wallnode/Wiedas_SensorApp_Movement.c
+++ example/contiki_wallnode/Wiedas_SensorApp_Movement.c
@@ -7,16 +7,22 @@
 #include "atmega_sdds_impl.h"
 
 #include <contiki.h>
+#include <clock.h>
 
 #include "MovementSensor-ds.h"
 
 #include "AMN31112.h"
 #include "GPIO_Input.h"
 
+
 #define MOVEMENT_isMoving 0
 #define MOVEMENT_notMoving 1
 
 #define MOVEMENT_EVENT_MOVEING 1
+
+static struct etimer movement_detection_timer;
+
+#if 1
 
 PROCESS(wiedas_movement_callback_handler, "WieDAS Movement IRQ handler");
 
@@ -26,6 +32,8 @@ static GPIO_Input g_gpio_PIR;
 
 void PIR_CallBack_handler( GPIO_Input _this, bool_t state);
 
+void _publishMovement(bool_t value);
+
 rc_t Wiedas_SensorApp_Movement_init() {
 
 	rc_t ret;
@@ -33,10 +41,10 @@ rc_t Wiedas_SensorApp_Movement_init() {
 	//ret = AMN31112_init();
 
 	// use gpio input as driver
-	struct GPIO_Input_t pir_input = {
-		.pin = 5,
+	static struct GPIO_Input_t pir_input = {
+		.pin = 0,
 		.bank = GPIO_INPUT_ATMEGA_BANK_B,
-		.mode = GPIO_INPUT_MODE_INTERUPT_ENABLE | GPIO_INPUT_MODE_CALLBACK_LEVEL_TRIGGER
+		.mode = GPIO_INPUT_MODE_INTERUPT_ENABLE
 	};
 	g_gpio_PIR = &pir_input;
 
@@ -55,6 +63,7 @@ rc_t Wiedas_SensorApp_Movement_init() {
 	}
 
 	// init the callback handler process
+	process_start(&wiedas_movement_callback_handler, NULL);
 
 
 
@@ -64,11 +73,35 @@ rc_t Wiedas_SensorApp_Movement_init() {
 rc_t Wiedas_SensorApp_Movement_start() {
 	return SDDS_RT_OK;
 }
+
+rc_t _publishMovement(bool_t value) {
+	rc_t ret;
+
+	// publish regular data
+		MovementSensor data;
+
+		data.id = nodeID;
+		if (value == true) {
+			data.state = MOVEMENT_isMoving;
+		} else {
+			data.state = MOVEMENT_notMoving;
+		}
+
+		DDS_ReturnCode_t ddsret;
+
+		ddsret = DDS_MovementSensorDataWriter_write(g_MovementSensor_writer, &data, NULL);
+
+		if (ddsret != DDS_RETCODE_OK) {
+			return SDDS_RT_FAIL;
+		}
+
+		return SDDS_RT_OK;
+}
 rc_t Wiedas_SensorApp_Movement_dowork() {
 
 
 	// read sensor
-	bool_t movement;
+	bool_t movement = 0;
 
 	//AMN31112_readMovement(&movement);
 
@@ -78,26 +111,8 @@ rc_t Wiedas_SensorApp_Movement_dowork() {
 
 	Log_debug("checked movement: %d \n", movement);
 
+	return _publishMovement(movement);
 
-	// publish regular data
-	MovementSensor data;
-
-	data.id = nodeID;
-	if (movement == true) {
-		data.state = MOVEMENT_isMoving;
-	} else {
-		data.state = MOVEMENT_notMoving;
-	}
-
-	DDS_ReturnCode_t ddsret;
-
-	ddsret = DDS_MovementSensorDataWriter_write(g_MovementSensor_writer, &data, NULL);
-
-	if (ddsret != DDS_RETCODE_OK) {
-		return SDDS_RT_FAIL;
-	}
-
-	return SDDS_RT_OK;
 }
 rc_t Wiedas_SensorApp_Movement_process() {
 	return SDDS_RT_OK;
@@ -105,9 +120,11 @@ rc_t Wiedas_SensorApp_Movement_process() {
 
 void PIR_CallBack_handler( GPIO_Input _this, bool_t state) {
 
-	uint8_t event = MOVEMENT_EVENT_MOVEING;
+//	uint8_t event = MOVEMENT_EVENT_MOVEING;
 
-	process_post(&wiedas_movement_callback_handler, event, (void*) _this);
+	//Log_debug("Something moved state %d \n", state);
+
+	process_post(&wiedas_movement_callback_handler, 23, (void*) _this);
 
 }
 
@@ -117,7 +134,37 @@ PROCESS_THREAD(wiedas_movement_callback_handler, ev, data)
 
   while(1) {
     PROCESS_WAIT_EVENT();
+
+    do {
+    	static bool_t prevalue = 0;
+
+
+
+    	GPIO_Input_getState(g_gpio_PIR, &prevalue);
+
+    	GPIO_Input_deactivateInterrupt(g_gpio_PIR);
+
+        etimer_set(&movement_detection_timer, CLOCK_SECOND / 10);
+
+        PROCESS_YIELD_UNTIL(etimer_expired(&movement_detection_timer));
+
+    	bool_t value2;
+    	GPIO_Input_getState(g_gpio_PIR, &value2);
+
+
+    	if (prevalue == value2) {
+    		Log_debug("Something moved %d \n", value2);
+    		_publishMovement(value2);
+    	}
+
+    	GPIO_Input_activateInterrupt(g_gpio_PIR);
+
+
+    } while (0);
+
   }
 
   PROCESS_END();
 }
+
+#endif
