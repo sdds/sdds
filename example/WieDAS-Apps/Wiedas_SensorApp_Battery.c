ac7c89c6378593880d2383eb840fc7dda6c058e7
Kai Beckmann <kai.beckmann@hs-rm.de>
2013-04-22 15:29:12 +0000

    led bliks if battery volatge is low
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@352 08239170-4572-482a-b791-2244fd6e1e4b

--- example/WieDAS-Apps/Wiedas_SensorApp_Battery.c
+++ example/WieDAS-Apps/Wiedas_SensorApp_Battery.c
@@ -12,6 +12,7 @@
 #include <dds/DDS_DCPS.h>
 
 #include <os-ssal/NodeConfig.h>
+#include "LED.h"
 
 
 #include "atmega_sdds_impl.h"
@@ -19,16 +20,20 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#include <stdint.h>
 
+#include "contiki.h"
+
+#define BATTERY_INA_COUNT 1
 extern SSW_NodeID_t nodeID;
-
+extern LED g_statusled;
+/*  
 volatile static uint8_t adc_data;
-
 ISR(ADC_vect) //ADC End of Conversion interrupt
 {
 	adc_data = ADC >> 2; //read 8 bit value
 }
-
+*/
 rc_t Wiedas_SensorApp_Battery_init() {
 
 	// manuall init of the stuff, there is not enough time for a driver ...
@@ -49,7 +54,7 @@ rc_t Wiedas_SensorApp_Battery_init() {
 			// enable ADC
 			_BV(ADEN) |
 			// enable IRQ if conversion is ready
-			_BV(ADIE) |
+//			_BV(ADIE) |
 			// division factor 128 ?
 			_BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2) |
 			// start first conversion
@@ -63,18 +68,72 @@ rc_t Wiedas_SensorApp_Battery_start() {
 }
 rc_t Wiedas_SensorApp_Battery_dowork() {
 
+	static	bool_t lowBattery = false;
+	static uint8_t inactiveCount = 0;
 
-	// start conversion
-	ADCSRA |= _BV(ADSC);
+
+	// check only all BATTERY_INA_COUNT seconds
+	// or if the battery is low, than do it every second
+	if (lowBattery == false && inactiveCount < BATTERY_INA_COUNT) {
+		inactiveCount++;
+
+		return SDDS_RT_OK;
+	} else {
+		inactiveCount = 0;
+	}
+
+
+	uint16_t sum = 0;
+
+//	clock_time_t t_start = 	clock_time();
+
+	for (uint8_t i = 0; i < 10; i++) {
+
+		// start conversion
+		ADCSRA |= _BV(ADSC);
+
+		while (ADCSRA & _BV(ADSC) ) {   // auf Abschluss der Konvertierung warten
+  		}
+
+		sum +=	ADC >> 2;
+	}
+//	Log_debug("sum %d\n", sum);
+
+	uint8_t adc_data = (uint8_t) (sum / 10);
+
+//	clock_time_t t_end = clock_time();
+
+//	Log_debug("Measure time t1 %d t2 %d   d_t %d \n",t_start, t_end, (t_end - 
+//	t_start) );
 
 	//get last result
 	// calculate value
-	// volt_val = V_measu * 255 / V_ref
+	// value = 1,8/255 * adc_val
 	// since V_ref it 1,8V volt_val is a problem
+	//
+	
+	uint8_t batteryVolt = (uint8_t) ((uint16_t)(71 * adc_data) / 100) ;
 
-	float_t test = adc_data * 255 / 1.8;
 
-	Log_debug("Bat: adc_data %d test %f \n", adc_data, test);
+	if (batteryVolt < 180) {
+		// low battery
+		lowBattery = true;
+		Log_warn("Battery is below 1,8V! It has %u V/100 \n ", batteryVolt);
+		// publish data
+		//
+		// toggle status led
+		LED_toggle(g_statusled);
+	} else  {
+		Log_debug("Battery voltage is >  %u V/100 \n", batteryVolt );
+	}
+
+	if (batteryVolt < 172 ) {
+	//	publish shutdown
+		
+		// shutdown
+
+	}
+
 
 	return SDDS_RT_OK;
 }
