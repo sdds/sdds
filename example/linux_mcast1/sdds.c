f6133c7f2bb4e58053ffa57b327ebcbbd465abd8
Olga Dedi <olga@dedi.de>
2015-05-14 22:51:25 +0000

    BuiltinTopic: Nahricht wird jetzt vollständig empfangen
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@387 08239170-4572-482a-b791-2244fd6e1e4b

--- example/linux_mcast1/sdds.c
+++ example/linux_mcast1/sdds.c
@@ -9,31 +9,36 @@
 int main()
 {
     pthread_t t;
-
 	sDDS_init();
     Log_setLvl(0);
 
-	for (;;) {
-
+    for (;;) {
         do
 		{
-			static uint8_t foo = 67;
+			//static uint8_t foo = 67;
             DDS_DCPSParticipant p;
-            p.key = foo;
+            p.key = 22;
            
             DDS_DCPSTopic t;
-            t.key = foo;
+            memset(t.name, 0, DDS_TOPIC_NAME_SIZE);
+            memset(t.type_name, 0, DDS_TOPIC_TYPE_SIZE);
+            t.key = 33;
             t.name[0] = 'T';
             t.name[1] = 'e';
             t.name[2] = 'm';
             t.name[3] = 'p';
-            t.name[4] = '\0';
-            t.type_name[0] = 'T';
-            t.type_name[1] = '1';
+            t.name[4] = '1';
+            t.name[5] = '2';
+            t.name[6] = '3';
+            t.name[7] = '\0';
+            t.type_name[0] = '4';
+            t.type_name[1] = '5';
             t.type_name[2] = '\0';
 
             Beta b;
-            b.value = foo++;
+            b.value = 44;
+            memset(b.value2, 0, 11);
+            memset(b.value3, 0, 10);
             b.value2[0] = 'D';
             b.value2[1] = 'e';
             b.value2[2] = 'r';
@@ -46,15 +51,17 @@ int main()
             b.value3[5] = '\0';
 
 
-
 			if (DDS_DCPSParticipantDataWriter_write(g_DCPSParticipant_writer, &p, NULL) != DDS_RETCODE_OK) {
-                // handle error
-            }
+				// handle error
+				//printf("-----DEBUG-----\t %s:%d\n", __FILE__, __LINE__);
+			}
             if (DDS_DCPSTopicDataWriter_write(g_DCPSTopic_writer, &t, NULL) != DDS_RETCODE_OK) {
                 // handle error
+            	//printf("-----DEBUG-----\t %s:%d\n", __FILE__, __LINE__);
             }
             if (DDS_BetaDataWriter_write(g_Beta_writer, &b, NULL) != DDS_RETCODE_OK) {
                 // handle error
+            	//printf("-----DEBUG-----\t %s:%d\n", __FILE__, __LINE__);
             }
             sleep(10);
 
