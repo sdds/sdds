a049d3a988e043e3084fada29a9c3fe43bec54b7
Andreas Zoor <andreaszoor@gmail.com>
2015-10-09 10:58:47 +0000

    merge port to Riot manually.
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@663 08239170-4572-482a-b791-2244fd6e1e4b

---
+++ platform/riot/NodeConfig.c
@@ -0,0 +1,17 @@
+#include "os-ssal/NodeConfig.h"
+
+#include "gen_constants.h"
+
+#include "cpuid.h"
+
+#ifndef PLATFORM_RIOT_SDDS_IFACE
+#define PLATFORM_RIOT_SDDS_IFACE 0
+#endif
+
+SSW_NodeID_t NodeConfig_getNodeID(void) {
+	SSW_NodeID_t nodeID = 0;
+	uint32_t cpuid;
+	cpuid_get(&cpuid);
+	nodeID = cpuid & 0xFFFF;
+	return nodeID;
+}
