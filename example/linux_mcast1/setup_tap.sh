17546150b79f831cc9ac8fe187ac9ec125379e55
Kevin Sapper <mail@kevinsapper.de>
2015-11-12 13:08:10 +0000

    Fixed mcast1
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@685 08239170-4572-482a-b791-2244fd6e1e4b

--- example/linux_mcast1/setup_tap.sh
+++ example/linux_mcast1/setup_tap.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
-
-tunctl -t tap0 -u $1
-tunctl -t tap1 -u $1
+ip tuntap add name tap0 mode tap
 echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
 sysctl net.ipv6.conf.tap0.accept_dad=0
-sysctl net.ipv6.conf.tap1.accept_dad=0
-ifconfig tap0 up
-ifconfig tap1 up
+ip link set tap0 up
 ip -6 addr add fe80::10/64 dev tap0
-ip -6 addr add fe80::20/64 dev tap0
-ip -6 addr add fe80::10/64 dev tap1
-ip -6 addr add fe80::20/64 dev tap1
-
