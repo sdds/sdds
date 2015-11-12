81d559f6284176d214494bd0f5e5757fdc7e97e1
Steffen Reichmann <reichmann.steffen@googlemail.com>
2015-11-12 13:55:21 +0000

    changed user from static to generic
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@687 08239170-4572-482a-b791-2244fd6e1e4b

--- example/linux_mcast1/setup_tap.sh
+++ example/linux_mcast1/setup_tap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ip tuntap add name tap0 mode tap
+ip tuntap add name tap0 mode tap user $USER
 echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
 sysctl net.ipv6.conf.tap0.accept_dad=0
 ip link set tap0 up
