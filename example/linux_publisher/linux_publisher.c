a5e0ca42d12517beda773b61259e56d6c21589a8
Kevin Sapper <mail@kevinsapper.de>
2015-11-19 08:01:16 +0000

    Problem: linxu Publisher and Subscriber have been damaged by accident Soltution: Use skeleton generation for examples for publisher and subscriber
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@729 08239170-4572-482a-b791-2244fd6e1e4b

--- example/linux_publisher/linux_publisher.c
+++ example/linux_publisher/linux_publisher.c
@@ -1,29 +1,9 @@
 #include "linux_publisher_sdds_impl.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <inttypes.h>
 
 int main()
 {
-	DDS_ReturnCode_t ret;
-
 	sDDS_init();
 	Log_setLvl(0);
-
-    Beta beta_pub;
-    beta_pub.value = 'H';
-    beta_pub.value2[0] = 'H';
-    beta_pub.value2[1] = 'W';
-    beta_pub.value3[0] = 'H';
-    beta_pub.value3[1] = 'W';
-
-    for (;;) {
-        ret = DDS_BetaDataWriter_write (g_Beta_writer, &beta_pub, NULL);
-        if (ret != DDS_RETCODE_OK)
-            printf ("Failed to send topic beta\n");
-
-        sleep (1);
-    }
 
     return 0;
 }
--- example/linux_subscriber/linux_subscriber.c
+++ example/linux_subscriber/linux_subscriber.c
@@ -1,37 +1,9 @@
 #include "linux_subscriber_sdds_impl.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <inttypes.h>
 
 int main()
 {
-	DDS_ReturnCode_t ret;
-
 	sDDS_init();
 	Log_setLvl(0);
-
-    Beta beta_sub;
-    Beta *beta_sub_p = &beta_sub;
-
-    for (;;) {
-        ret = DDS_BetaDataReader_take_next_sample(g_Beta_reader,
-                &beta_sub_p, NULL);
-        if (ret == DDS_RETCODE_NO_DATA) {
-            printf("no data for beta\n");
-        }
-        else {
-            printf("Received a sample from topic 'beta': {\n"
-                   "   value => %c\n"
-                   "   value2 => %s\n"
-                   "   value3 => %s\n"
-                   "}\n"
-                   , beta_sub_p->value
-                   , beta_sub_p->value2
-                   , beta_sub_p->value3);
-        }
-
-        sleep (1);
-    }
 
     return 0;
 }
