c1018257a7aecdf8f14176039783ebd2292d0411
Kevin Sapper <mail@kevinsapper.de>
2015-11-19 12:30:39 +0000

    Problem: linux subcriber and publisher got messed up somehow Solution: use skeleton generation to restore them
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@735 08239170-4572-482a-b791-2244fd6e1e4b

--- example/linux_publisher/linux_publisher.c
+++ example/linux_publisher/linux_publisher.c
@@ -1,9 +1,23 @@
 #include "linux_publisher_sdds_impl.h"
+#include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
+	DDS_ReturnCode_t ret;
+
 	sDDS_init();
 	Log_setLvl(0);
+
+    Ipc ipc_pub;
+
+    for (;;) {
+        ret = DDS_IpcDataWriter_write (g_Ipc_writer, &ipc_pub, NULL);
+        if (ret != DDS_RETCODE_OK)
+            printf ("Failed to send topic ipc\n");
+
+        sleep (1);
+    }
 
     return 0;
 }
--- example/linux_subscriber/linux_subscriber.c
+++ example/linux_subscriber/linux_subscriber.c
@@ -1,9 +1,29 @@
 #include "linux_subscriber_sdds_impl.h"
+#include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
+	DDS_ReturnCode_t ret;
+
 	sDDS_init();
 	Log_setLvl(0);
+
+    Ipc ipc_sub;
+    Ipc *ipc_sub_p = &ipc_sub;
+
+    for (;;) {
+        ret = DDS_IpcDataReader_take_next_sample(g_Ipc_reader,
+                &ipc_sub_p, NULL);
+        if (ret == DDS_RETCODE_NO_DATA) {
+            printf("no data for ipc\n");
+        }
+        else {
+            printf("Received (ipc)\n");
+        }
+
+        sleep (1);
+    }
 
     return 0;
 }
