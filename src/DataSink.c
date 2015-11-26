7ae8c5fba4fcc784e86fad3f731927e75c0164e6
Andreas Zoor <andreaszoor@gmail.com>
2015-11-26 11:24:28 +0100

    add debug and error log informations.

--- src/DataSink.c
+++ src/DataSink.c
@@ -50,6 +50,9 @@ DataSink_getTopic (DDS_DCPSSubscription *st, topicid_t id, Topic_t **topic) {
 	for (index = 0; index < SDDS_DATA_READER_MAX_OBJS; index++) {
 		if (BitArray_check (&self->allocated_readers, index)
         &&  DataReader_topic (&self->readers[index])->id == id) {
+		Log_debug("search for Topic %d in reader with id %d\n", id, DataReader_id (dataSink->readers[index]));
+		if (dataSink->readers[index]
+        &&  DataReader_topic (dataSink->readers[index])->id == id) {
 			if (st != NULL) {
 				st->key = DataReader_id (&self->readers[index]);
 				st->participant_key = BuiltinTopic_participantID;
@@ -98,7 +101,7 @@ rc_t DataSink_processFrame(NetBuffRef_t *buff) {
 
 		NetBuffRef_renew(buff);
 		Log_error("invalid SNPS headder\n");
-		return SDDS_RT_FAIL;
+		return SDDS_RT_FAIL;	
 	}
 
 	// should be NULL!
