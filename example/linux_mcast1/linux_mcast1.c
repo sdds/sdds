5453c913e56da1c876f184d0afe7cec115937958
Olga Dedi <olga@dedi.de>
2015-11-27 11:50:46 +0100

    linux_mcast1 must be beta :-P

--- example/linux_mcast1/linux_mcast1.c
+++ example/linux_mcast1/linux_mcast1.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "linux_mcast1_sdds_impl.h"
 
 int main()
@@ -6,17 +7,18 @@ int main()
 
 	sDDS_init();
 	Log_setLvl(0);
- 
-    Ipc ipc_pub;
-    ipc_pub.value = 0xAFFE;
+
+    Beta beta_pub;
+    beta_pub.value = 'H';
+    strncpy (beta_pub.value2,   "Es gibt im", 10);
+    strncpy (beta_pub.value3,   "Es gibt i", 9);
 
     for (;;) {
-        ret = DDS_IpcDataWriter_write (g_Ipc_writer, &ipc_pub, NULL);
-        if (ret != DDS_RETCODE_OK){
-            printf ("Failed to send topic ipc\n");
-        }else{
-            printf ("Send: %d\n", ipc_pub.value);
-        }
+        ret = DDS_BetaDataWriter_write (g_Beta_writer, &beta_pub, NULL);
+        if (ret != DDS_RETCODE_OK) {
+            printf ("Failed to send topic beta\n");
+		}
+
         sleep (1);
     }
 
