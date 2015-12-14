417e0043c0764b4d67537eda8c5d604a55721499
Andreas Zoor <andreaszoor@gmail.com>
2015-12-14 15:03:01 +0100

    Implement the new mutex interface for autobest.

---
+++ platform/autobest/Mutex.c
@@ -0,0 +1,88 @@
+/*
+ * Mutex.h
+ *
+ *  Created on: Dec 14, 2015
+ *      Author: andreas
+ */
+
+#include "os-ssal/SSW.h"
+#include "os-ssal/Mutex.h"
+#include "os-ssal/Memory.h"
+#include "Log.h"
+
+#include <api_calls.h>
+#include "arch/sys_arch.h"
+
+#define UNLOCK_STATE    0
+#define LOCK_STATE      1
+
+struct Mutex{
+    uint8_t old_prio;
+    uint8_t state;
+};
+typedef struct Mutex Mutex_t;
+
+/**
+ * -----------------------------------------------------------------------------
+ * Creates a mutes object
+ *
+ * @return  Returns the new mutex object.
+ */
+Mutex_t*
+Mutex_create(void) {
+    Mutex_t* mutex = Memory_alloc(sizeof(Mutex_t));
+    return (Mutex_t*) mutex;
+}
+
+/**
+ * -----------------------------------------------------------------------------
+ * Initializes the mutex.
+ *
+ * @param[in]   Mutex which should be initialized.
+ * @return      Returns SDDS_SSW_RT_OK on success and SDDS_SSW_RT_FAIL on failure.
+ */
+ssw_rc_t
+Mutex_init(Mutex_t* mutex) {
+    mutex->state = UNLOCK_STATE;
+    return SDDS_SSW_RT_OK;
+}
+
+/**
+ * -----------------------------------------------------------------------------
+ * Locks the mutex.
+ *
+ * @param[in]   Mutex which should be locked.
+ * @return      Returns SDDS_SSW_RT_OK on success and SDDS_SSW_RT_FAIL on failure.
+ */
+ssw_rc_t
+Mutex_lock(Mutex_t* mutex) {
+    if(mutex == NULL){
+        return SDDS_SSW_RT_FAIL;
+    }
+    if(mutex->state == LOCK_STATE){
+        return SDDS_SSW_RT_OK;
+    }
+    mutex->old_prio =  sys_arch_protect();
+    mutex->state = LOCK_STATE;
+    return SDDS_SSW_RT_OK;
+}
+
+/**
+ * -----------------------------------------------------------------------------
+ * Unlocks the mutex.
+ *
+ * @param[in]   Mutex which should be unlocked.
+ * @return     Returns SDDS_SSW_RT_OK on success and SDDS_SSW_RT_FAIL on failure.
+ */
+ssw_rc_t
+Mutex_unlock(Mutex_t* mutex) {
+    if(mutex == NULL){
+        return SDDS_SSW_RT_FAIL;
+    }
+    if(mutex->state == UNLOCK_STATE){
+        return SDDS_SSW_RT_OK;
+    }
+    sys_arch_unprotect(mutex->old_prio);
+    mutex->state = UNLOCK_STATE;
+    return SDDS_SSW_RT_OK;
+}
