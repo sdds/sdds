a4c97b4f81e23c077a2bd73e7a71c6cb05edbe05
Andreas Zoor <andreaszoor@gmail.com>
2015-12-15 11:46:30 +0100

    Make comments in the to specify potantional trace points.

--- src/History.c
+++ src/History.c
@@ -51,6 +51,7 @@ rc_t
 sdds_History_enqueue(History_t* self, NetBuffRef_t* buff) {
     assert(self);
     assert(buff);
+    // TODO: Trace point prossesFrame
     //  Queue is full.
     if (self->in_needle == self->depth) {
         return SDDS_RT_FAIL;
