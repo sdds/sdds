9e5d71d5e99f2cf2022343c9f0c2591f14821ea9
Kevin Sapper <mail@kevinsapper.de>
2015-12-18 11:13:37 +0100

    Problem: Line number is not printed in log messages Solution: Added a line number to each log message

--- platform/contiki/Log.c
+++ platform/contiki/Log.c
@@ -1,19 +1,11 @@
-/*
- * =====================================================================================
- *
- *       Filename:  Log.c
- *
- *    Description:  impl for the log class
- *
- *        Version:  1.0
- *        Created:  22.02.2010 16:21:39
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Kai Beckmann (kai), kai-oliver.beckmann@hs-rm.de
- *        Company:  Hochschule RheinMain - DOPSY Labor für verteilte Systeme
- *
- * =====================================================================================
+/*  =========================================================================
+    Log - Contiki logging implementation
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.
+
+    This file is part of sDDS:
+    http://wwwvs.cs.hs-rm.de.
+    =========================================================================
  */
 
 #include "Log.h"
@@ -38,92 +30,86 @@ Log_setLvl(int lvl) {
 }
 #ifndef UTILS_NO_LOGGING
 void
-_log_trace(const char* fnk, ...) {
+_log_trace(const char* fnk, int line, ...) {
     if (logLvl < 1 ) {
         va_list arg;
         char* string;
-        va_start(arg, fnk);
+        va_start(arg, line);
 
         string = va_arg(arg, char*);
-        PRINTF("TRACE: %s() : ", fnk);
-
-        vprintf(string, arg);
+        fprintf(stderr, "T: %s()[%d]: ", fnk, line);
+        vprintf(stderr, string, arg);
         va_end(arg);
     }
 }
 
 void
-_log_debug(const char* fnk, ...) {
+_log_debug(const char* fnk, int line, ...) {
     if (logLvl < 2 ) {
         va_list arg;
         char* string;
-        va_start(arg, fnk);
+        va_start(arg, line);
 
         string = va_arg(arg, char*);
-        PRINTF("DEBUG: %s() : ", fnk);
-
-        vprintf(string, arg);
+        fprintf(stderr, "D: %s()[%d]: ", fnk, line);
+        vprintf(stderr, string, arg);
         va_end(arg);
     }
 }
 
 void
-_log_info(const char* fnk, ...) {
+_log_info(const char* fnk, int line, ...) {
     if (logLvl < 3) {
         va_list arg;
         char* string;
-        va_start(arg, fnk);
+        va_start(arg, line);
 
         string = va_arg(arg, char*);
-
-        PRINTF("INFO: %s() : ", fnk);
-        vprintf(string, arg);
+        fprintf(stderr, "I: %s()[%d]: ", fnk, line);
+        vprintf(stderr, string, arg);
 
         va_end(arg);
     }
 }
 
 void
-_log_warn(const char* fnk, ...) {
+_log_warn(const char* fnk, int line, ...) {
     if (logLvl < 4) {
         va_list arg;
         char* string;
-        va_start(arg, fnk);
+        va_start(arg, line);
 
         string = va_arg(arg, char*);
-        PRINTF("WARN: %s() : ", fnk);
-
-        vprintf(string, arg);
+        fprintf(stderr, "W: %s()[%d]: ", fnk, line);
+        vprintf(stderr, string, arg);
         va_end(arg);
     }
 }
 
 void
-_log_error(const char* fnk, ...) {
+_log_error(const char* fnk, int line, ...) {
     if (logLvl < 5) {
         va_list arg;
         char* string;
-        va_start(arg, fnk);
+        va_start(arg, line);
 
         string = va_arg(arg, char*);
-        PRINTF("ERROR: %s() : ", fnk);
-
-        vprintf(string, arg);
+        fprintf(stderr, "E: %s()[%d]: ", fnk, line);
+        vprintf(stderr, string, arg);
         va_end(arg);
     }
 }
 
 void
-_log_panic(const char* fnk, ...) {
+_log_panic(const char* fnk, int line, ...) {
     if (logLvl < 6) {
         va_list arg;
         char* string;
-        va_start(arg, fnk);
+        va_start(arg, line);
 
         string = va_arg(arg, char*);
-        PRINTF("PANIC: %s() : ", fnk);
-
-        vprintf(string, arg);
+        fprintf(stderr, "P: %s()[%d]: ", fnk, line);
+        vprintf(stderr, string, arg);
         va_end(arg);
         abort();
     }
