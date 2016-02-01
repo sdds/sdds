40fb3271a50830c4918b662fbf4bea5f488c70b1
Andreas Zoor <andreaszoor@gmail.com>
2016-02-01 19:36:42 +0100

    new version of the example.

--- example/linux_autobest/linux_autobest.c
+++ example/linux_autobest/linux_autobest.c
@@ -20,13 +20,15 @@ int main()
     alpha_pub.value = 0xAA;
 
     for (;;) {
-        ret = DDS_AlphaDataWriter_write (g_Alpha_writer, &alpha_pub, NULL);
-        if (ret != DDS_RETCODE_OK){
-            printf ("Failed to send topic aplha\n");
-        }
+
         ret = DDS_IpcDataWriter_write (g_Ipc_writer, &ipc_pub, NULL);
         if (ret != DDS_RETCODE_OK){
             printf ("Failed to send topic ipc\n");
+        }
+        //sleep(1);
+        ret = DDS_AlphaDataWriter_write (g_Alpha_writer, &alpha_pub, NULL);
+        if (ret != DDS_RETCODE_OK){
+            printf ("Failed to send topic aplha\n");
         }
         DDS_ReturnCode_t ret = DDS_BetaDataReader_take_next_sample(g_Beta_reader, &recv_beta_addr, NULL);
         if (ret == DDS_RETCODE_OK){
