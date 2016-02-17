13c90c84268b5bf77cc0e85cb8371c069b14db03
Andreas Zoor <andreaszoor@gmail.com>
2016-02-17 18:52:25 +0100

    new analyse script for analyse different tracing mdoes.

--- example/autobest_qemu/sdds.c
+++ example/autobest_qemu/sdds.c
@@ -1,78 +1,55 @@
 
 #include <stdio.h>
-#include <sdds_sdds_impl.h>
+#include "sdds_sdds_impl.h"
 #include <ipc-ds.h>
+//#include <beta-ds.h>
 #include <os-ssal/Thread.h>
 #include <os-ssal/Trace.h>
 #include <Log.h>
-
 #include <hv.h>
-#include <sdds_server_shm.h>
-#include <dds_obj_ids.h>
-#include <app.id.h>
 
-#define SLEEP_TIMEOUT_NSEC 10000000000ULL
+#define SLEEP_TIMEOUT_NSEC 50000000ULL
 
-void
-sdds_rpc_sdds_app1(unsigned int reply_id, unsigned long dds_obj_id);
+rc_t
+shm_server_init();
 
+int main()
+{
+	DDS_ReturnCode_t ret;
 
-struct {
-	char s[1024];
-} __stack_rpc_sdds_app1 __aligned(16);
+	sDDS_init();
+	//Log_setLvl(0);
 
-int main(void);
+    Ipc ipc_pub;
+    ipc_pub.value = 0xAFFE;
 
-int main(void){
-    rc_t ret = sDDS_init();
-    if(ret != SDDS_RT_OK){
-        sys_task_terminate();
-        sys_abort();
-        return -1;
+    for (;;) {
+
+        ret = DDS_IpcDataWriter_write (g_Ipc_writer, &ipc_pub, NULL);
+#ifdef FEATURE_SDDS_TRACING_ENABLED
+        Trace_point(SDDS_TRACE_EVENT_DUMMY_1);
+        Trace_point(SDDS_TRACE_EVENT_DUMMY_2);
+#ifdef FEATURE_SDDS_TRACING_SEND_ISOLATED
+        Trace_point(SDDS_TRACE_EVENT_DUMMY_3);
+        Trace_point(SDDS_TRACE_EVENT_DUMMY_4);
+        Trace_point(SDDS_TRACE_EVENT_DUMMY_5);
+        Trace_point(SDDS_TRACE_EVENT_DUMMY_6);
+        Trace_point(SDDS_TRACE_EVENT_DUMMY_7);
+#endif
+        Trace_point(SDDS_TRACE_EVENT_STOP);
+#endif
+        if (ret != DDS_RETCODE_OK){
+            printf ("Failed to send topic ipc\n");
+        }
+        sys_sleep(SLEEP_TIMEOUT_NSEC);
     }
-	Log_setLvl(3);
-	printf("Initilaized sDDS\n");
 
-	sys_task_terminate();
+    sys_task_terminate();
     sys_abort();
 	return 0;
 }
 
-void
-sdds_rpc_sdds_app1(unsigned int reply_id, unsigned long dds_obj_id){
-    //select on witch dds reader or writer the rpc ist called
-
-#ifdef FEATURE_SDDS_TRACING_ENABLED
-#ifdef FEATURE_SDDS_TRACING_RPC_TASK
-    Trace_point(SDDS_TRACE_EVENT_RPC_TASK);
-#endif
-#endif
-    switch(dds_obj_id){
-        case DDS_IPC_WRITER_ID:
-            if(g_shm_sdds_app1->ipc_seg.type == 1){
-                Ipc* data_used_ptr = &g_shm_sdds_app1->ipc_seg.data;
-                DDS_ReturnCode_t ret = DDS_IpcDataWriter_write(g_Ipc_writer, data_used_ptr, NULL);
-#ifdef FEATURE_SDDS_TRACING_ENABLED
-#ifdef FEATURE_SDDS_TRACING_RPC_REPLY
-                Trace_point(SDDS_TRACE_EVENT_RPC_REPLY);
-#endif
-#endif
-                sys_rpc_reply(reply_id, (unsigned long)ret, 1);
-                sys_task_terminate();
-            	assert(0);
-            }
-            //TODO: if sdds implment more function of the dds standart then we need to check more types for the DataWirte.
-            break;
-    }
-#ifdef FEATURE_SDDS_TRACING_ENABLED
-#ifdef FEATURE_SDDS_TRACING_RPC_REPLY
-    Trace_point(SDDS_TRACE_EVENT_RPC_REPLY);
-#endif
-#endif
-    sys_rpc_reply(reply_id, (unsigned long)-1, 1);
-    /* if RPC reply returns, the caller's partition was rebooted */
-    sys_task_terminate();
-	assert(0);
+rc_t
+shm_server_init(){
+    return SDDS_RT_OK;
 }
-
-
