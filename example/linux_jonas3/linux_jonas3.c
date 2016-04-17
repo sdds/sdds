f9ee1d29c2d0f8abbdf38c10f11d413fcfe5ef51
Andreas Zoor <andreaszoor@gmail.com>
2016-04-17 11:47:26 +0200

    example3

--- example/linux_jonas3/linux_jonas3.c
+++ example/linux_jonas3/linux_jonas3.c
@@ -3,6 +3,7 @@
 
 int main() {
 	DDS_ReturnCode_t ret;
+  Beta beta_pub;
 
 	if (sDDS_init() == SDDS_RT_FAIL) {
 		return 1;
@@ -10,9 +11,16 @@ int main() {
 
 	Log_setLvl(0);
 
+  sleep(1);
+  Security_receive_key();
+
+  beta_pub.value = 'H';  
+  strncpy(beta_pub.value2,   "Es gibt im", 10);
+  strncpy(beta_pub.value3,   "Es gibt i", 9);
+
   for (;;) {
-    Security_receive_key();
-    sleep (1);
+    ret = DDS_BetaDataWriter_write(g_Beta_writer, &beta_pub, NULL);
+    sleep (5);
   }
 
   return 0;
