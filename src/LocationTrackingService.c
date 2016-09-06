331ce5546ff27b83072effa9a32e2222cfc682a7
Olga Dedi <olga@dedi.de>
2016-09-06 21:22:59 +0200

    pass tracked devices as cli argument

--- src/LocationTrackingService.c
+++ src/LocationTrackingService.c
@@ -31,7 +31,7 @@ static Task s_updateTask;
 static void
 s_updateDevices(void* obj);
 
-ssw_rc_t
+rc_t
 LocationTrackingService_init() {
     memset(&s_devicePool, 0, SDDS_LOCATION_TRACKING_MAX_DEVS * sizeof(struct DeviceLocation));
     s_freeDevs = SDDS_LOCATION_TRACKING_MAX_DEVS;
@@ -47,10 +47,10 @@ LocationTrackingService_init() {
         return SDDS_SSW_RT_FAIL;
     }
 
-    return SDDS_SSW_RT_OK;
+    return SDDS_RT_OK;
 }
 
-ssw_rc_t
+rc_t
 LocationTrackingService_trackDevice(SSW_NodeID_t device) {
     assert(device);
 
@@ -58,13 +58,13 @@ LocationTrackingService_trackDevice(SSW_NodeID_t device) {
 
     while (NULL != dev) {
         if (dev->device == device) {
-            return SDDS_SSW_RT_OK;
+            return SDDS_RT_OK;
         }
         dev = (DeviceLocation_t*) s_trackedDevices->next_fn(s_trackedDevices);
     }
 
     if (0 == s_freeDevs) {
-        return SDDS_SSW_RT_FAIL;
+        return SDDS_RT_FAIL;
     }
 
     int freePos = -1;
@@ -76,7 +76,7 @@ LocationTrackingService_trackDevice(SSW_NodeID_t device) {
     }
 
     if (-1 == freePos) {
-        return SDDS_SSW_RT_FAIL;
+        return SDDS_RT_FAIL;
     }
 
     s_devicePool[freePos].device = device;
@@ -85,13 +85,13 @@ LocationTrackingService_trackDevice(SSW_NodeID_t device) {
     if (SDDS_RT_FAIL == ret) {
 
         s_devicePool[freePos].device = 0;
-        return SDDS_SSW_RT_FAIL;
+        return SDDS_RT_FAIL;
     }
 
-    return SDDS_SSW_RT_OK;
+    return SDDS_RT_OK;
 }
 
-ssw_rc_t
+rc_t
 LocationTrackingService_untrackDevice(SSW_NodeID_t device) {
     assert(device);
 
@@ -103,14 +103,14 @@ LocationTrackingService_untrackDevice(SSW_NodeID_t device) {
                 memset(dev, 0, sizeof(struct DeviceLocation));
                 return SDDS_SSW_RT_OK;
             }
-            return SDDS_SSW_RT_FAIL;
+            return SDDS_RT_FAIL;
         }
         dev = (DeviceLocation_t*) s_trackedDevices->next_fn(s_trackedDevices);
     }
-    return SDDS_SSW_RT_FAIL;
+    return SDDS_RT_FAIL;
 }
 
-ssw_rc_t
+rc_t
 LocationTrackingService_getDeviceLocation(SSW_NodeID_t device, DeviceLocation_t* devLoc) {
     assert(device);
     assert(devLoc);
@@ -123,7 +123,7 @@ LocationTrackingService_getDeviceLocation(SSW_NodeID_t device, DeviceLocation_t*
         }
         dev = (DeviceLocation_t*) s_trackedDevices->next_fn(s_trackedDevices);
     }
-    return SDDS_SSW_RT_FAIL;
+    return SDDS_RT_FAIL;
 }
 
 List_t*
@@ -136,7 +136,7 @@ s_updateDevices(void* obj) {
     DeviceLocation_t* dev = (DeviceLocation_t*) s_trackedDevices->first_fn(s_trackedDevices);
     while (NULL != dev) {
         ssw_rc_t ret = LocationService_getDeviceLocation(dev->device, dev);
-        if (SDDS_SSW_RT_OK != ret) {
+        if (SDDS_RT_OK != ret) {
             Log_error("Obtaining location failed for device %x\n", dev->device);
         }
         dev = (DeviceLocation_t*) s_trackedDevices->next_fn(s_trackedDevices);
